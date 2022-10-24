884ba5563840caf27416815711c9abf272ffa89d
leoruas <leo.coelho.ruas@gmail.com>
2022-10-23 22:53:55 -0300

    heroku test

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"os"
 
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
 	server "github.com/octaviomuller/kendamais-server/internal"
 	"github.com/octaviomuller/kendamais-server/internal/config"
 	"github.com/octaviomuller/kendamais-server/internal/controller"
@@ -17,10 +16,6 @@ import (
 var connectionString string
 
 func getEnv() {
-	if err := godotenv.Load(".env"); err != nil {
-		log.Fatal("No .env file found")
-	}
-
 	connectionString = os.Getenv("POSTGREE_URI")
 	if connectionString == "" {
 		log.Fatal("You must set your 'POSTGREE_URI' env variable.")
